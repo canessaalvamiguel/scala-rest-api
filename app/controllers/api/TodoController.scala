997b8f1a0764a914f7af0d7ca914e2145c7dc873
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-11-19 11:57:10 -0500

    Add more endpoints

--- app/controllers/api/TodoController.scala
+++ app/controllers/api/TodoController.scala
@@ -21,4 +21,41 @@ class TodoController @Inject()(cc : ControllerComponents, todoService: TodoServi
     }
   }
 
+  def getById(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+    todoService.getItem(id) map { item =>
+      Ok(Json.toJson(item))
+    }
+  }
+
+  def add() = Action.async { implicit request: Request[AnyContent] =>
+    TodoForm.form.bindFromRequest.fold(
+      // if any error in submitted data
+      errorForm => {
+        errorForm.errors.foreach(println)
+        Future.successful(BadRequest("Error!"))
+      },
+      data => {
+        val newTodoItem = Todo(0, data.name, data.isComplete)
+        todoService.addItem(newTodoItem).map( _ => Redirect(routes.TodoController.getAll))
+      })
+  }
+
+  def update(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+    TodoForm.form.bindFromRequest.fold(
+      // if any error in submitted data
+      errorForm => {
+        errorForm.errors.foreach(println)
+        Future.successful(BadRequest("Error!"))
+      },
+      data => {
+        val todoItem = Todo(id, data.name, data.isComplete)
+        todoService.updateItem(todoItem).map( _ => Redirect(routes.TodoController.getAll))
+      })
+  }
+
+  def delete(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+    todoService.deleteItem(id) map { res =>
+      Redirect(routes.TodoController.getAll)
+    }
+  }
 }
